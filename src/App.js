04fa9044672d13e3ea2c132a5f8d3f36be3c2d58
Abu3rida <khaledabuarida0@gmail.com>
2022-09-28 03:25:20 +0200

    fixed the storage problem

--- src/App.js
+++ src/App.js
@@ -1,49 +1,40 @@
-import NoteList from './Component/NoteList'
 import { useEffect , useState } from 'react'
 import { nanoid } from 'nanoid'
+import NoteList from './Component/NoteList'
 import Search from './Component/Search'
 import Header from './Component/Head'
-
 
 function App(){
 
   // INITIALZING NOTE LIST STATE 
-  const [note, setNote] = useState([
-    {
-      id: nanoid(),
-      title: 'Science',
-      text: 'this is a science note here some sort of text more and more',
-      date: '20/5/2014'
-    },
-    {
-      id: nanoid(),
-      title: 'Economic',
-      text: 'this is a content about economics here some sort of text more and more',
-      date: '16/3/2018'
-    },
-    {
-      id: nanoid(),
-      title: 'Studying',
-      text: 'this is a studying content',
-      date: '25/9/2015'
-    }
-  ])
+  const [note, setNote] = useState(
+    ()=>{
+      const getSavedNotes = localStorage.getItem('saved-notes');
+      const intialNotes = JSON.parse(getSavedNotes);
+      return intialNotes || ''
+      
+    })
+
   //SAVING TO LOCAL STORAGE
 
-  useEffect(()=>{
-    const note = JSON.parse(localStorage.getItem('note'));
+    useEffect(()=>{
+      localStorage.setItem('saved-notes',JSON.stringify(note));
+    },[note]);
 
-    if(note){
-      setNote(note);
-    }
+  // useEffect(()=>{
+  //   const note = JSON.parse(localStorage.getItem('note'));
+
+  //   if(note){
+  //     setNote(note);
+  //   }
       
-  },[]);
+  // },[]);
 
-  useEffect(()=>{
-    if(note.length>0){
-      localStorage.setItem('note',JSON.stringify(note))
-    }
-  },[note]);
+  // useEffect(()=>{
+  //   if(note.length>0){
+  //     localStorage.setItem('note',JSON.stringify(note))
+  //   }
+  // },[note]);
 
 
   // ADDING NOTE 
@@ -82,6 +73,7 @@ function App(){
           modeState={darkMode}
         />
         <Search  handleSearchText={setSearchText}/>
+        <addNote/>
         <NoteList 
           note={note.filter((note) => note.text.toLowerCase().includes(searchText.toLowerCase()))} 
           generateNote={addNote} 
